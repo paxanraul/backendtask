f2988a2dc736ccf5653ef9135959b41d3ffbe478
paxanraul <raul20081033@gmail.com>
2026-05-19 17:49:02 +0300

    add get_current_user dependency

---
+++ app/dependencies/auth.py
@@ -0,0 +1,55 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from sqlalchemy.orm import Session
+
+from app.db.session import get_db
+from app.core.security import decode_access_token
+from app.models.user import User
+from app.models.token_blacklist import TokenBlackList
+
+bearer_scheme = HTTPBearer()
+
+
+def get_current_user(
+        credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
+        db: Session = Depends(get_db)
+) -> User:
+    
+    token = credentials.credentials
+
+    in_blacklist = db.query(TokenBlackList).filter(
+        TokenBlackList.token == token
+    )
+
+
+    payload = decode_access_token(token)
+    if payload is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Невалидный или просроченный токен"
+        )
+    
+
+    user_id: str = payload.get("sub")
+    if user_id is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Невалдиный токен"
+        )
+    
+
+    user = db.query(User).filter(User.id == int(user_id)).first()
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Пользователь не найден"
+        )
+    
+
+    if not user.is_active:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Аккаунт деактивирован"
+        )
+    
+    return user
