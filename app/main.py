fdb02eea72b150c334b44c8dda6ef61c03fe1cc6
paxanraul <raul20081033@gmail.com>
2026-05-19 18:18:57 +0300

    include users router

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from app.routers import auth
+from app.routers import auth, users
 from app.models import user, role, permission, token_blacklist 
 
 app = FastAPI(title="Backend", version="1.0.0")
 
 app.include_router(auth.router)
+app.include_router(users.router)
 
 
 @app.get("/health")
