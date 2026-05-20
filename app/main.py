8697b6a2a4a4ba9fe55329502273097e0b570ad4
paxanraul <raul20081033@gmail.com>
2026-05-20 14:32:47 +0300

    connect to mock routers

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from app.routers import auth, users
+from app.routers import auth, users, mock
 from app.models import user, role, permission, token_blacklist 
 from app.db.seed import run_seed
 from app.db.session import SessionLocal
@@ -18,6 +18,7 @@ app = FastAPI(title="Backend", version="1.0.0")
 
 app.include_router(auth.router)
 app.include_router(users.router)
+app.include_router(mock.router)
 
 
 @app.get("/health")
