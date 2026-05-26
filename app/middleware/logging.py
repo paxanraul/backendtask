3d75f36cb2f700e2cd008b8c5c94084325120d36
paxanraul <raul20081033@gmail.com>
2026-05-26 21:23:36 +0300

    add middleware logs

---
+++ app/middleware/logging.py
@@ -0,0 +1,19 @@
+import time
+import logging
+from fastapi import Request
+
+logger = logging.getlogger(__name__)
+
+
+async def log_requets(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = round((time.time() - start_time) * 1000, 2)
+
+    logger.info(
+        f"{request.method} {request.url.path} "
+        f"status={response.status_code} "
+        f"time={process_time}ms"
+    )
+
+    return response
