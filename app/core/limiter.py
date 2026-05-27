192c9ee65dbc0701e60dea2a19847f7eadd6ca32
paxanraul <raul20081033@gmail.com>
2026-05-27 20:48:22 +0300

    disable rate limiting for tests

--- app/core/limiter.py
+++ app/core/limiter.py
@@ -1,4 +1,8 @@
+import os
 from slowapi import Limiter
 from slowapi.util import get_remote_address
 
-limiter = Limiter(key_func=get_remote_address)
+limiter = Limiter(
+    key_func=get_remote_address,
+    enabled=os.environ.get("TESTING") != "true"
+    )
