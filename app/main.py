dfdcde9c64d2c7de4173622bf925087d904926ad
paxanraul <raul20081033@gmail.com>
2026-05-27 20:41:38 +0300

    connect to limiter

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from slowapi import _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
 from app.routers import auth, users, mock, admin
 from app.models import user, role, permission, token_blacklist 
 from app.db.seed import run_seed
 from app.db.session import SessionLocal
 from app.middleware.logging import log_requests
+from app.core.limiter import limiter
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -17,6 +20,8 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(title="Backend", version="1.0.0", lifespan=lifespan)
 
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.middleware("http")(log_requests)
 
 app.include_router(auth.router)
